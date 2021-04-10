0c252f6a9255c17b738a769d5bc61c52de96c663
koop9087 <yarik4mo>
2021-04-11 01:17:08 +0300

    update

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,9 +1,8 @@
 public class Calculate {
     public static int[] findPositiveElemsOfMatrix(int[][] array) {
-        int size = findSizeOfMas(array);
+        int size = findSizeOfPositiveElemsOnMainDiagonal(array);
         int[] mas = new int[size];
-        int k = 0;
-        for (int i = 0, j = 0; i < array.length && j < array[i].length; i++, j++) {
+        for (int i = 0, j = 0, k = 0; i < array.length && j < array[i].length; i++, j++) {
             if (array[i][j] > 0) {
                 mas[k] = array[i][j];
                 k++;
@@ -12,7 +11,7 @@ public class Calculate {
         return mas;
     }
 
-    private static int findSizeOfMas(int[][] array) {
+    private static int findSizeOfPositiveElemsOnMainDiagonal(int[][] array) {
         int count = 0;
         for (int i = 0, j = 0; i < array.length && j < array[i].length; i++, j++) {
             if (array[i][j] > 0) {
